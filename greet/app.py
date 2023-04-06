4fd87035db76c17b9bb8d926c5fd5557f6807dda
Nikki <dominiquelcruz@gmail.com>
2023-04-05 19:43:29 -0700

    greet answers commited

---
+++ greet/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/welcome')
+def welcome():
+    return "Welcome!"
+
+@app.route('/welcome/home')
+def welcomehome():
+    return "Welcome home!"
+
+@app.route('/welcome/back')
+def welcomeback():
+    return "Welcome back!"
+
